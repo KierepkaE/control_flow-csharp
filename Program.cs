fa816bf7ebb4589c5712c40961581accbbe7afe1
kierepkae <kierepkaedyta@gmail.com>
2019-11-21 15:19:15 +0100

    add while loop that prints out even numbers from 0 to 10

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace Conditionals {
       } else {
         Console.WriteLine ("It's evening");
       }
+      var i = 0;
+      while (i <= 10) {
+        if (i % 2 == 0) {
+          Console.WriteLine (i);
+        }
+        i++;
+      }
+
     }
   }
 }
